e704d3e678bd3425d5b472b9726bef7c0809cfa4
dipika <avatidipika18@gmail.com>
2023-03-11 22:43:37 +0530

    sign-up funcationality implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingInPageComponent } from './sing-in-page/sing-in-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sing-in-page/sing-in-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sing-in-page',
+  templateUrl: './sing-in-page.component.html',
+  styleUrls: ['./sing-in-page.component.css']
+})
+export class SingInPageComponent {
+
+}
